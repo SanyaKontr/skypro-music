17bd30c63751dd136486ad76d6c446c37ffe2b61
SanyaKontr <Bs13111@mail.ru>
2023-11-13 04:25:13 +0300

    hw react

---
+++ music/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p className="text">
+        Здесь скоро будет Skypro.Music!
+        </p>
+        <a
+          //className="App-link"
+         //href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+      
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
