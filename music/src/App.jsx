4445f90d6353d743341a007db911014e728c0614
SanyaKontr <Bs13111@mail.ru>
2023-11-15 17:51:33 +0300

    HwProps

---
+++ music/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import AudioPlayer from './components/AudioPlayer.jsx';
+import NavMenu from './components/NavMenu.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import Tracklist from './components/Tracklist.jsx';
+import Search from './components/Search.jsx';
+import Filters from './components/Filters.jsx';
+
+function App() {
+  return (
+    <> <div className="wrapper">
+      <div className="container">
+        <main className="main">
+         <NavMenu />
+          <div className="main__centerblock centerblock">
+            <Search />
+            <h2 className="centerblock__h2">Треки</h2>
+            <Filters />
+            <Tracklist />
+            </div>
+          <Sidebar />
+        </main>
+        <AudioPlayer />
+        <footer className="footer"></footer>
+      </div>
+    </div>
+    </>
+
+  );
+}
+
+export default App;
