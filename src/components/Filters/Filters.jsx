3bd26dd9ce5b93b0b9f958d62455b4d3734b4a94
SanyaKontr <Bs13111@mail.ru>
2023-11-27 15:10:13 +0300

    Промежуток 2

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,18 +1,65 @@
-import './Filters.css'
+import { useState } from "react";
 
-function Filters () {
-  return  (
-           <div className="centerblock__filter filter">
-              <div className="filter__title">Искать по:</div>
-              <div className="filter__button button-author _btn-text">
-                исполнителю
-              </div>
-              <div className="filter__button button-year _btn-text">
-                году выпуска
-              </div>
-              <div className="filter__button button-genre _btn-text">жанру</div>
-            </div>
-    );
+import './Filters.css'
+import {
+  OpenFilterSinger,
+  OpenFilterYears,
+  OpenFilterGenre,
+} from "./ActiveFilters.jsx";
+
+function Filters() {
+  const [openFilter, setOpenFilter] = useState(null);
+
+  const toggleFilter = (filter) => {
+    setOpenFilter((prevFilter) => (prevFilter === filter ? null : filter));
+  };
+
+  return (
+    <div className="centerblock__filter filter">
+      <div className="filter__title">Искать по:</div>
+      <OpenFilterSinger
+        buttonText="исполнителю"
+        listItems={[
+          "Nero",
+          "Dynoro, Outwork, Mr. Gee",
+          "Ali Bakgor",
+          "Стоункат, Psychopath",
+          "Jaded, Will Clarke, AR/CO",
+          "Blue Foundation, Zeds Dead",
+          "HYBIT, Mr. Black, Offer Nissim, Hi Profile",
+          "minthaze",
+          "Calvin Harris, Disciples",
+          "Tom Boxer",
+          "Calvin Harris, Disciples",
+        ]}
+        isOpen={openFilter === "исполнителю"}
+        toggleFilter={() => toggleFilter("исполнителю")}
+        isSelected={true}
+      />
+      <OpenFilterYears
+        buttonText="году выпуска"
+        listItems={["1992", "1993", "1994", "1995"]}
+        isOpen={openFilter === "году выпуска"}
+        toggleFilter={() => toggleFilter("году выпуска")}
+        isSelected={true}
+      />
+      <OpenFilterGenre
+        buttonText="жанру"
+        listItems={[
+          "Рок",
+          "Хип-хоп",
+          "Поп-музыка",
+          "Техно",
+          "Инди",
+          "Метал",
+          "Классическая музыка",
+        ]}
+        isOpen={openFilter === "жанру"}
+        toggleFilter={() => toggleFilter("жанру")}
+        isSelected={true}
+      />
+    </div>
+  );
 }
 
 export default Filters;
